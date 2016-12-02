81b72ce452fd91dc79cb0d423a2c53ba9241788b
Nitin Kumar <Nitin Kumar>
2016-12-02 22:41:37 +0530

    woeid updated

--- index.py
+++ index.py
@@ -1,42 +1,3 @@
-'''from TwitterSearch import *
-from twitter import *
-import urllib
-
-r = urllib.urlopen('https://api.twitter.com/1.1/trends/place.json?id=1').read()
-print r
-
-
-
-
-
-
-
-
-try:
-    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['Guttenberg', 'Doktorarbeit']) # let's define all words we would like to have a look for
-    tso.set_language('de') # we want to see German tweets only
-    tso.set_include_entities(False) # and don't give us all those entity information
-
-    # it's about time to create a TwitterSearch object with our secret tokens
-    ts = TwitterSearch(
-        consumer_key = 'UtvgXDJeHGZoL8naPRBVQJBTU',
-        consumer_secret = 'xUO1RzRoIqQBP5pMhiscLBDyRH9cLEUtw8WtgZ9RvFI721MR8I',
-        access_token = '2955186811-3knD17GyGB21G1obeECLiMA5NsJTNU1tkeBG94J',
-        access_token_secret = '7Ba84Alidfz9nAZWcb33EFW2DmeCyxr9SJoXvVYyEkzDx'
-     )
-
-     # this is where the fun actually starts :)
-    for tweet in ts.search_tweets_iterable(tso):
-        print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) ).encode('utf-8')
-
-except TwitterSearchException as e: # take care of all those ugly errors if there are some
-    print(e)
-
-
-
-'''
-
 from twitter import *
 import urllib
 import json
@@ -92,16 +53,13 @@ numberOfTweets = 10
 with open('woeid.json') as data_file:
     woeid_list = json.load(data_file)
 
-length = len(woeid_list['array'])
+length = len(woeid_list)
 
-
+print length
 for i in range(0,length):
-    try:    
-        print str(woeid_list['array'][i]['parentid'])
-        r = t.trends.place(_id = woeid_list['array'][i]['parentid']) 
-        print r
-    except:
-        print "error"
-
-
-#getTrends()
+        try:    
+            print str(woeid_list[i]['country_name'])
+            r = t.trends.place(_id = woeid_list[i]['woeid']) 
+            print r
+        except:
+            print "error"
